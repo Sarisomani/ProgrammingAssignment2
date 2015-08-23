b61a1e85dc414b6a1105097d2818145ea254b4af
Sarisomani <saraswati_talukdar@yahoo.co.in>
2015-08-22 19:05:29 -0500

    Revised Prog Assignment 2
    V2

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,12 @@
+# Matrix inversion is usually a costly computation and there may be some benefit
+# to caching the inverse of a matrix rather than compute it repeatedly. The
+# following two functions are used to cache the inverse of a matrix.
+
+# makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
   m<-NULL
   set<-function(y){
@@ -11,6 +20,12 @@ makeCacheMatrix <- function(x = matrix()) {
        setmatrix=setmatrix,
        getmatrix=getmatrix)
 }
+# The following function returns the inverse of the matrix. It first checks if
+# the inverse has already been computed. If so, it gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache via
+# setinverse function.
+
+# This function assumes that the matrix is always invertible.
 
 cacheSolve <- function(x=matrix(),...) 
 {    
@@ -24,12 +39,12 @@ cacheSolve <- function(x=matrix(),...)
   x$setmatrix(m)
   m
 }
+## Sample run:
+##A<-matrix(c(2,0,0,2),nrow=2,ncol=2)
+##ob1<-makeCacheMatrix(A)
+##ob2<-cacheSolve(ob1)
+##ob2
 
-A<-matrix(c(2,0,0,2),nrow=2,ncol=2)
-ob1<-makeCacheMatrix(A)
-ob2<-cacheSolve(ob1)
-ob2
-
-ob1$set(matrix(c(1,0,0,1),nrow=2,ncol=2))
-ob2<-cacheSolve(ob1)
-ob2
+##ob1$set(matrix(c(1,0,0,1),nrow=2,ncol=2))
+##ob2<-cacheSolve(ob1)
+##ob2
